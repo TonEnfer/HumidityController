8911aa46a15142a84423cbff9fae6b96ff65479a
TonEnfer <tonik.sam@gmail.com>
2018-02-14 21:28:07 +0500

    Add GPIO driver and Interrupts.cpp

---
+++ inc/HAL/Drivers/GPIO.cpp
@@ -0,0 +1,17 @@
+/*
+ * GPIO.cpp
+ *
+ *  Created on: 12 февр. 2018 г.
+ *      Author: anton.samoylov
+ */
+
+#include <HAL/Drivers/GPIO.h>
+
+namespace HAL {
+
+GPIO::GPIO() {
+	// TODO Auto-generated constructor stub
+
+}
+
+} /* namespace HAL */
---
+++ inc/interrupts.cpp
@@ -0,0 +1,65 @@
+/*
+ * interrupts.cpp
+ *
+ *  Created on: 13 פוגנ. 2018 ד.
+ *      Author: anton.samoylov
+ */
+
+#include <interrupts.h>
+void Reset_Handler(void) // גûחûגאועסÿ ןנט סבנמסו ןנמצא
+{
+
+
+}
+
+void NMI_Handler(void)
+{
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+
+  /* USER CODE END NonMaskableInt_IRQn 1 */
+}
+
+/**
+* @brief This function handles Hard fault interrupt.
+*/
+void HardFault_Handler(void)
+{
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+
+  /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+  }
+  /* USER CODE BEGIN HardFault_IRQn 1 */
+
+  /* USER CODE END HardFault_IRQn 1 */
+}
+
+/**
+* @brief This function handles System service call via SWI instruction.
+*/
+void SVC_Handler(void)
+{
+  /* USER CODE BEGIN SVC_IRQn 0 */
+
+  /* USER CODE END SVC_IRQn 0 */
+  /* USER CODE BEGIN SVC_IRQn 1 */
+
+  /* USER CODE END SVC_IRQn 1 */
+}
+
+/**
+* @brief This function handles Pendable request for system service.
+*/
+void PendSV_Handler(void)
+{
+  /* USER CODE BEGIN PendSV_IRQn 0 */
+
+  /* USER CODE END PendSV_IRQn 0 */
+  /* USER CODE BEGIN PendSV_IRQn 1 */
+
+  /* USER CODE END PendSV_IRQn 1 */
+}
