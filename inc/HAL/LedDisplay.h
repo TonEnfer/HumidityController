86e5eeff67dbb97f61e3f73c9e3b4f7533fe04ac
TonEnfer <tonik.sam@gmail.com>
2018-02-17 14:55:17 +0500

    Допилил драйвер дисплея
    Требует дальнейшей доработки

--- inc/HAL/LedDisplay.h
+++ inc/HAL/LedDisplay.h
@@ -8,14 +8,34 @@
 #ifndef LEDDISPLAY_H_
 #define LEDDISPLAY_H_
 #include <main.h>
+
 namespace HAL {
 
 class LedDisplay {
+protected:
+	uint16_t numArray[11] = {
+			0b10000011001110, //0
+			0b10000000000010, //1
+			0b10000001101100, //2
+			0b10000001101010, //3
+			0b10000010100010, //4
+			0b00000011101010, //5
+			0b00000011101110, //6
+			0b10000001000010, //7
+			0b10000011101110, //8
+			0b10000011101010, //9
+			0b0000000000000 //10
+
+			};
+
 public:
 	LedDisplay();
 	void init();
 	void show(uint8_t);
 	void off();
+	uint16_t numbers[2] = {0,0};
+	uint8_t curNum = 0;
+	uint32_t clearmask = (uint32_t)0b0000011101110<<16;
 };
 extern LedDisplay Display;
 }
--- src/HAL/LedDisplay.cpp
+++ src/HAL/LedDisplay.cpp
@@ -6,18 +6,50 @@
  */
 
 #include <HAL/LedDisplay.h>
+
+extern "C" void TIM3_IRQHandler() {
+	TIM3->SR &= ~0xFFFF;
+	uint16_t num = 	HAL::Display.numbers[0];
+	GPIOA->BSRR = HAL::Display.clearmask;
+	GPIOA->BSRR = num;
+	GPIOA->BSRR = GPIO_BSRR_BS_4;
+}
+
 namespace HAL {
 LedDisplay::LedDisplay() {
 
 }
 
 void LedDisplay::init() {
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+	GPIOA->MODER |= (GPIO_MODER_MODER1_0 | GPIO_MODER_MODER2_0
+			| GPIO_MODER_MODER3_0 | GPIO_MODER_MODER5_0 | GPIO_MODER_MODER6_0
+			| GPIO_MODER_MODER7_0 | GPIO_MODER_MODER4_0);
+	GPIOA->OSPEEDR |= ( GPIO_OSPEEDER_OSPEEDR1 | GPIO_OSPEEDER_OSPEEDR2
+			| GPIO_OSPEEDER_OSPEEDR3 | GPIO_OSPEEDER_OSPEEDR5 |
+			GPIO_OSPEEDER_OSPEEDR6 | GPIO_OSPEEDER_OSPEEDR7
+			| GPIO_OSPEEDER_OSPEEDR4);
+
+	GPIOA->BSRR = clearmask;
+
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+	TIM3->DIER |= TIM_DIER_UIE;
+	TIM3->PSC = (uint16_t) 7; //6MHz
+	TIM3->ARR = (uint16_t) 0xFFFF; // ~11ms
+	TIM3->EGR |= TIM_EGR_UG;
+	TIM3->CR1 |= TIM_CR1_ARPE | TIM_CR1_URS | TIM_CR1_CEN;
+
+	NVIC_SetPriority(TIM3_IRQn, 1);
+	NVIC_EnableIRQ(TIM3_IRQn);
 
 }
 
 void LedDisplay::show(uint8_t number) {
-	if (number > 99) {
+	if (number > 10) {
+		numbers[0] = numArray[number%10];
 	} else {
+		numbers[0] = numArray[number];
 	}
 }
 
